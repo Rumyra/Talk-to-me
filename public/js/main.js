2f311b2946b09d9a82b7e8ac16e3aac78bd0a037
SeanHolden <sean.holden@o2.com>
2013-08-29 15:33:47 +0100

    completed ruby backend and javascript to a point where you can click speech button and it plays audio.

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ $(function(){
 
 function initializeSpeakButton(){
   $("input[value='Speak']").click(function(){
+    $("input[value='Speak']").replaceWith("<img id=\"loader\" src=\"/img/loader.gif\" style=\"float:right;\" />");
     var currentTextValue = $('#speaktext').val();
     playTextAsAudio();
     return false;
@@ -13,6 +14,7 @@ function initializeSpeakButton(){
 }
 
 function playTextAsAudio(){
+
   var text = $('#speaktext').val();
   $.ajax({
     type: "POST",
@@ -28,4 +30,8 @@ function createAudioElement(url){
   var audio_element = "<audio id=\"player\"><source src=\""+url+"\" type=\"audio/mpeg\" />Your browser does not support the audio element.</audio>"
   $('#audio_container').html(audio_element);
   $('#player')[0].play();
+  // $("#loader").replaceWith("<input type=\"submit\" value=\"Speak\" />");
+  $('#speak_button_container').load('/ #speak_button_container', function(){
+    initializeSpeakButton();
+  });
 }
